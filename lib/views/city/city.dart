351f2d8ccec526092e747448b38c31e11335e085
dymafr <contact@dyma.fr>
2021-05-27 14:18:57 +0200

    first commit

---
+++ lib/views/city/city.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import './widgets/trip_activity_list.dart';
+
+import './widgets/activity_list.dart';
+import './widgets/trip_overview.dart';
+
+import '../../models/activity.model.dart';
+import '../../models/trip.model.dart';
+import '../../datas/data.dart' as data;
+
+class City extends StatefulWidget {
+  final List<Activity> activities = data.activities;
+
+  @override
+  _CityState createState() => _CityState();
+}
+
+class _CityState extends State<City> {
+  late Trip mytrip;
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+    mytrip = Trip(activities: [], date: null, city: 'Paris');
+  }
+
+  List<Activity> get tripActivities {
+    return widget.activities
+        .where((activity) => mytrip.activities.contains(activity.id))
+        .toList();
+  }
+
+  void setDate() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now().add(Duration(days: 1)),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2020),
+    ).then((newDate) {
+      if (newDate != null) {
+        setState(() {
+          mytrip.date = newDate;
+        });
+      }
+    });
+  }
+
+  void switchIndex(newIndex) {
+    setState(() {
+      index = newIndex;
+    });
+  }
+
+  void toggleActivity(String id) {
+    setState(() {
+      mytrip.activities.contains(id)
+          ? mytrip.activities.remove(id)
+          : mytrip.activities.add(id);
+    });
+  }
+
+  void deleteTripActivity(String id) {
+    setState(() {
+      mytrip.activities.remove(id);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.chevron_left),
+        title: Text('Organisation voyage'),
+        actions: <Widget>[
+          Icon(Icons.more_vert),
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            TripOverview(
+              trip: mytrip,
+              setDate: setDate,
+            ),
+            Expanded(
+              child: index == 0
+                  ? ActivityList(
+                      activities: widget.activities,
+                      selectedActivities: mytrip.activities,
+                      toggleActivity: toggleActivity,
+                    )
+                  : TripActivityList(
+                      activities: tripActivities,
+                      deleteTripActivity: deleteTripActivity,
+                    ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Découverte',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.stars),
+            label: 'Mes activités',
+          )
+        ],
+        onTap: switchIndex,
+      ),
+    );
+  }
+}
